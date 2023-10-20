fdba10c7a4867196896e6d9775b443d8e198ae6b
Olivier DRAGHI <odraghi@gmail.com>
2023-10-20 18:48:19 +0200

    Add some args

--- install_kubernetes.sh
+++ install_kubernetes.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Copyright (C) 2023 DRAGHI Olivier
-#     This program comes with ABSOLUTELY NO WARRANTY; for details type `install_kubernetes.sh help'.
+#     This program comes with ABSOLUTELY NO WARRANTY; for details type `install_kubernetes.sh --help'.
 #     This is free software, and you are welcome to redistribute it
-#     under certain conditions; type `install_kubernetes.sh conditions' for details.
+#     under certain conditions; type `install_kubernetes.sh --copyright' for details.
 
 QUANTITY_OF_K8S_VERSIONS=5
 QUANTITY_OF_CPU_MIN=2
 QUANTITY_OF_MEMORY_GB_MIN=2
 
+THIS_PROGRAM_VERSION=1.0.0
+THIS_PROGRAM=$0
 
 function copyright()
 {
@@ -30,6 +32,101 @@ function copyright()
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>
 EOF
+}
+
+function this_version()
+{
+   echo "Program: ${THIS_PROGRAM} version ${THIS_PROGRAM_VERSION}"
+}
+
+function this_help()
+{
+   cat << EOF
+
+    Usage: ${THIS_PROGRAM} [OPTIONS] [kubernetes-version]
+
+DESCRIPTION
+
+    This program install the prerequisites to run and work with the kubernetes.
+    By defaut the program list available kubernetes version and ask you which one you want.
+
+UNATTENDED INSTALL
+
+    There is three way for unattended installation:
+      ${THIS_PROGRAM} 1.27
+      ${THIS_PROGRAM} --latest
+      ${THIS_PROGRAM} --older 1.24 
+
+OPTIONS
+     --latest        Install the latest version of Kubernetes.
+
+     --older         Allow older version of available Kubernetes version.
+                     By default you can only install one of the ${QUANTITY_OF_K8S_VERSIONS} latest versions.
+
+     --skip-heml     Skip HELM install. You should use that for Production node.
+
+     -s, --slow      That let time for humans reading what's happened (INFO/WARNING messages)
+
+     -h, --help      Show this help.
+
+	  -v, --version       Show this program version.
+	  -c, --copyright     Show this program copyright.
+
+EOF
+}
+
+function parse_args()
+{
+   POSITIONAL_ARGS=()
+
+   while [[ $# -gt 0 ]]; do
+      case $1 in
+         # --my-argument)
+         #    ARG_MY_ARGUMENT="$2"
+         #    shift # past argument
+         #    shift # past value
+         #    ;;
+         --latest)
+            ARG_LATEST="yes"
+            shift # past argument
+            ;;
+         --older)
+            ARG_OLDER_K8S_VERSIONS="yes"
+            shift # past argument
+            ;;
+         --skip-helm)
+            ARG_SKIP_HELM="yes"
+            shift # past argument
+            ;;
+         -s|--slow)
+            DELAY_SECOND=2
+            shift # past argument
+            ;;
+         -h|--help)
+            this_help
+            exit
+            ;;
+         -v|--version)
+            this_version
+            exit
+            ;;
+         -c|--copyright)
+            copyright
+            exit
+            ;;
+         -*|--*)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+         *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            [ ${#POSITIONAL_ARGS[@]} -gt 1 ] && fatal_error "Unexpected positional args : ${POSITIONAL_ARGS[0]}"
+            ;;
+      esac
+   done
+
+   ARG_K8S_VERSION=${POSITIONAL_ARGS[0]}
 }
 
 function is_debian12()
@@ -58,28 +155,40 @@ function is_kubernetes_repo_exist()
 
 function select_kubernetes_version()
 { 
+  ([ ! -z ${ARG_K8S_VERSION} ] && [ ! -z ${ARG_LATEST} ]) && fatal_error "You can't request at the same time --latest and a specific older version."
+
+  [ ${ARG_OLDER_K8S_VERSIONS:-no} == yes ] && QUANTITY_LIMIT=100
+
   VERSIONS=$(apt-cache madison kubectl \
 		| awk '{print $3}' \
-		| awk -F "." 'BEGIN {VERSION=x} $2!= VERSION {print $1 "." $2 "." $3; VERSION = $2}'\
-		| head -${QUANTITY_OF_K8S_VERSIONS} \
+		| awk -F "." 'BEGIN {VERSION=x}   $2 != VERSION {print $1 "." $2 "." $3; VERSION = $2}'\
+		| head -${QUANTITY_LIMIT:-${QUANTITY_OF_K8S_VERSIONS}} \
 		| tac)
+
+  LATEST_VERSION_INDEX=$(echo ${VERSIONS} | wc -w)
+
   ARRAY_VERSIONS=()
   COUNT=1
   for VERSION in ${VERSIONS}; do
     ARRAY_VERSIONS+=(${VERSION})
-    [ ${COUNT} -eq ${QUANTITY_OF_K8S_VERSIONS} ] && LATEST_MESSAGE=" (latest)" || LATEST_MESSAGE=""
-    echo "${COUNT}) Kubernetes-$( echo ${VERSION} | sed 's/\.[0-9]*-.*$//')"${LATEST_MESSAGE}
+    [ ${COUNT} -eq ${LATEST_VERSION_INDEX} ] && MESSAGE=" (latest)" || MESSAGE=""
+    [ ! -z ${ARG_K8S_VERSION} ] && echo ${VERSION} | grep -q ${ARG_K8S_VERSION} \
+         && AUTO_INPUT=${COUNT} && MESSAGE="${MESSAGE} (requested version)"
+    echo "${COUNT}) Kubernetes-$( echo ${VERSION} | sed 's/\.[0-9]*-.*$//')"${MESSAGE}
     ((COUNT++))
   done
-
-  echo -e "Install (${QUANTITY_OF_K8S_VERSIONS}):\c "
-  read USER_INPUT
+ 
+  echo -e "Install (${LATEST_VERSION_INDEX}):\c "
+  [ ! -z ${ARG_K8S_VERSION} ] && [ -z ${AUTO_INPUT} ] && fatal_error "Kubernetes requested version not found. Maybe you need to increase the history of available version."
+  ([ ${ARG_LATEST:-no} == no ] && [ -z ${ARG_K8S_VERSION} ]) && read USER_INPUT  # Interactive input
+  [ ${ARG_LATEST:-no} == yes ] && echo ${LATEST_VERSION_INDEX}                   # Latest version
+  [ ! -z ${AUTO_INPUT} ] && USER_INPUT=${AUTO_INPUT} && echo ${AUTO_INPUT}       # Specific version
 
   # If input is string
   expr ${USER_INPUT} + 1 &> /dev/null
   [ $? -ne 0 ] && log_warn "Invalid input.." && USER_INPUT=${QUANTITY_OF_K8S_VERSIONS}
    
-  [ ${USER_INPUT} -gt ${QUANTITY_OF_K8S_VERSIONS} ] &> /dev/null
+  [ ${USER_INPUT} -gt ${LATEST_VERSION_INDEX} ] &> /dev/null
   [ $? -eq 0 ] && log_warn "Invalid input.." && USER_INPUT=${QUANTITY_OF_K8S_VERSIONS}
 
   [ -z ${USER_INPUT} ] && log_info "Using latest" # (-1) last element of the array of versions
@@ -116,8 +225,8 @@ kubernetes_prerequisites()
    disable_swap
 
    log_info "Checking CRI (Container Runtime Interface) for Kubernetes"
-   is_containerd_installed || install_containerd
-   link_kubernetes_cni_binaries_to_containerd_path
+   is_containerd_installed && log_info "containerd is already installed" || install_containerd
+   is_containerd_configured_for_kubernetes && log_info "containerd seems already configured with SystemdCgroup" || setup_containerd
    log_info "You can use contaired as CRI (Container Runtime Interface) for Kubernetes"
    
    setup_crictl_to_containerd
@@ -192,12 +301,6 @@ install_containerd()
    log_info "Installing Containerd"
    apt-get install -y containerd
    apt-mark hold containerd
-
-   [ ! -f /etc/containerd/config.toml.debian ] \
-      && log_info "Save debian config file. /etc/containerd/config.toml" \
-      && cp -p /etc/containerd/config.toml{,.debian}
-
-   setup_containerd
 }
 
 setup_crictl_to_containerd()
@@ -206,25 +309,40 @@ setup_crictl_to_containerd()
    crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock
 }
 
+is_containerd_configured_for_kubernetes()
+{
+   [ ! -f /etc/containerd/config.toml ] && return false
+   return $(grep -q "SystemdCgroup = true" /etc/containerd/config.toml)
+}
+
 setup_containerd()
 {
-   cp -p /etc/containerd/config.toml{,.ori}
+   [ ! -f /etc/containerd/config.toml.debian ] \
+      && log_info "Save debian config file. /etc/containerd/config.toml" \
+      && cp -p /etc/containerd/config.toml{,.debian}
 
-   log_info "Setup Containerd - Load default configuration attributs"
+   cp -p /etc/containerd/config.toml{,.backup-$(date "+%Y-%m-%d-%Hh%M")}
+
+   log_info "Containerd config - Load default configuration attributes"
    containerd config default > /etc/containerd/config.toml
+   
+   log_info "Containerd config - Comply Debian, bin_dir = \"/usr/lib/cni\""
+   sed -i "s/^\(.*bin_dir = \).*/\1\"\/usr\/lib\/cni\"/" /etc/containerd/config.toml
 
-   log_info "Setup Containerd - Configure systemd cgroup driver"
-   sed -i "s/^\( *SystemdCgroup =\).*/\1 true/" /etc/containerd/config.toml
-
-   log_info "Setup Containerd - Comply with Debian path /opt/containerd -> /var/lib/containerd/opt"
+   log_info "Containerd config - Comply Debian, io.containerd.internal.v1.opt : path = /var/lib/containerd/opt"
    sed -i "s/^\( *path =\) \"\/opt\/containerd\"/\1 \"\/var\/lib\/containerd\/opt\"/" /etc/containerd/config.toml
+   
+   log_info "Containerd config - Enable Systemd cgroup driver"
+   sed -i "s/^\( *SystemdCgroup =\).*/\1 true/" /etc/containerd/config.toml
 
    service containerd restart
 }
 
-link_kubernetes_cni_binaries_to_containerd_path()
+link_kubernetes_cni_to_containerd()
 {
-   log_info "Linking Kubernetes installed CNI to Containerd CNI directory: /var/lib/containerd/opt/bin (official debian path)"
+   log_info "Linking Kubernetes-cni to Containerd"
+   
+   [ ! -d /var/lib/containerd/opt/bin ] && mkdir -p /var/lib/containerd/opt/bin
    for CNI in /opt/cni/bin/* ; do ln -s -v --force ${CNI} /var/lib/containerd/opt/bin/; done
 }
 
@@ -235,6 +353,7 @@ is_hemld_installed()
 
 install_helm()
 {
+   log_info "Installing Heml"
    curl https://baltocdn.com/helm/signing.asc | gpg --dearmor --yes -o /usr/share/keyrings/helm.gpg
    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" \
          | tee /etc/apt/sources.list.d/helm-stable-debian.list
@@ -255,13 +374,13 @@ add_some_helm_repo()
 log_info()
 {
    echo -e "\nINFO: $*"
-   sleep 1
+   sleep ${DELAY_SECOND:-0}
 }
 
 log_warn()
 {
    echo -e "\nWARNING: $*"
-   sleep 1
+   sleep ${DELAY_SECOND:-0}
 }
 
 fatal_error()
@@ -272,33 +391,31 @@ fatal_error()
 
 ## Main
 
-if [ ! -z $1 ]; then
-   copyright
-   log_info "The script '$0' run without parameter"
-   log_info "Adding argument will simply show the copyright"
-   exit
-fi
+parse_args $*
 
 is_debian12 || fatal_error "This script is only tested for Debian12"
 
 [ ! is_kubernetes_repo_exist ] && setup_kubernetes_repo || apt-get update
 
 select_kubernetes_version
-echo "You are going to install packages for kubernetes ${VERSION_TO_INSTALL}"
+log_info "Installing kubernetes ${VERSION_TO_INSTALL}"
 
-is_kubernetes_pkg_installed && log_info "Packages are already installed"
+is_kubernetes_pkg_installed && log_info "kubernetes ${VERSION_TO_INSTALL} are already installed"
 if ! is_kubernetes_pkg_installed; then
    log_info "Need to replace kubernetes packages"
    apt-mark unhold	kubelet kubeadm kubectl
    apt remove -y	kubelet kubeadm kubectl
    apt install -y	kubelet=${VERSION_TO_INSTALL} kubeadm=${VERSION_TO_INSTALL} kubectl=${VERSION_TO_INSTALL}
    apt-mark hold	kubelet kubeadm kubectl
+   link_kubernetes_cni_to_containerd
 fi
 
 kubernetes_prerequisites
 
-is_hemld_installed || install_helm
-add_some_helm_repo
+if [ ${ARG_SKIP_HELM:-no} == no ]; then
+   is_hemld_installed || install_helm
+   add_some_helm_repo
+fi
 
 log_info "Installed Kubertenes packages"
 dpkg -l		kubelet kubeadm kubectl
